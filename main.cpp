ec081c8c63d03b9baeac757633ed2237d18086b0
Cheuk Lam Cheung <ccheung792423@gmail.com>
2020-10-29 12:06:22 -0400

    draft

--- main.cpp
+++ main.cpp
@@ -8,10 +8,12 @@ int main (){
   Coord3D b = {15,3,11};
   
   //Task A
+  std::cout << "Task A\n";
   std::cout << "For the coordinate, A, where\n   x=10, y=20, z=30:\nThe distance from the coordinate to the origin is " << length(&a) << std::endl;
 
   //Task B
-  std::cout << "\nCompared to the coordinate, B, where\n   x=15, y=3, z=11\n";
+  std::cout << "\nTask B\n";
+  std::cout << "Compared to the coordinate, B, where\n   x=15, y=3, z=11\n";
   if (fartherFromOrigin(&a,&b) == &a){
     std::cout << "Coordinate A is farther away from the origin\n";
   }
@@ -20,13 +22,15 @@ int main (){
   }
 
   //Task C
+  std::cout << "\nTask C";
   move(&a,&b,10);
     std::cout << "\nAfter 10 seconds of moving coordinate A by:\n   15 in the x direction\n   3 in the y direction\n   11 in the z direction\nThe current coordinate for A is\n   x=" << a.x << " y=" << a.y << " z=" << a.z << std::endl;
 
   //Task E
+    std::cout << "\nUsing Task E's method to create Coord3D for velocity\n";
   Coord3D *ppos = createCoord3D(5.5,-1.47,7.77);
   move(&a,ppos,10);
-  std::cout << "\nAfter another 10 second of moving corrdinate by:\n   5.5 in the x direction\n   -1.47 in the y direction\n   7.77 in the z direction\nThe Coordinate for A is now\n   x=" << a.x << " y=" << a.y << " z=" << a.z << std::endl; 
+  std::cout << "After another 10 second of moving corrdinate by:\n   5.5 in the x direction\n   -1.47 in the y direction\n   7.77 in the z direction\nThe Coordinate for A is now\n   x=" << a.x << " y=" << a.y << " z=" << a.z << std::endl; 
   deleteCoord3D(ppos);
 
   return 0;
